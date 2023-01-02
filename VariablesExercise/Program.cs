44f99727d0c4c8b8804d77cec4dcee6f3bf7b006
Matt Fuller <mnfuller552@gmail.com>
2023-01-02 14:53:50 -0500

    Finished Variable Exercise

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string dogName;
+            
+            dogName = "Rex";
+
+            int dogAge = 5;
+
+            char firstInitial = 'R';
+
+            bool isHungry = true;
+
+            double dogWeight = 54.3;
+
+            decimal dogHeight = 14.7m;
+
+            Console.WriteLine($"Hello. My dog's name is {dogName} and he is {dogAge} years old.");
+            Console.WriteLine($"He weighs {dogWeight} pounds and he is {dogHeight} inches tall.");
+            Console.WriteLine($"It is {isHungry.ToString().ToLower()}, he is always hungry.");
+            Console.WriteLine($"His name starts with the letter {firstInitial}.");
+
+
+
+
+
         }
     }
 }
